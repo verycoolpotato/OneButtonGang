d09d240ff88b9c35e1b43265863e13381b2a3527
verycoolpotato <100438125+verycoolpotato@users.noreply.github.com>
2025-09-03 18:13:31 -0700

    Added double jumping

--- OneButton/Assets/Scripts/PlayerController.cs
+++ OneButton/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,32 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D Rb;
     [SerializeField] private float MovementSpeed;
+    [SerializeField] private float JumpHeight;
 
+    [SerializeField] LayerMask GroundedLayers;
+    
     private int _dir = 1;
+    private int _jumps = 2;
+
+    private void Start()
+    {
+       
+    }
+
     private void FixedUpdate()
     {
         AutoRun(_dir);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
 
+        
+    }
 
     //Automatic Movement
     private void AutoRun(int direction)
@@ -27,5 +45,31 @@ public class PlayerController : MonoBehaviour
         direction = !direction;
 
         _dir = direction ? 1 : -1;
+    }
+
+    //Jump
+    private void Jump()
+    {
+
+        GroundedCheck();
+        if (_jumps > 0)
+        {
+            Rb.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
+
+            _jumps--;
+        }
+       
+    }
+
+    private void GroundedCheck()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, 0.1f, Vector2.down, 0.5f, GroundedLayers);
+       bool isGrounded = hit.collider != null;
+       
+
+       if(isGrounded)
+        {
+            _jumps = 2;
+        }
     }
 }
