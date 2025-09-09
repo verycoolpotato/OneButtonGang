4c2ab57efe2261b966c2ad60a220c9f39cf1d404
verycoolpotato <arandmagson@gmail.com>
2025-09-08 18:04:29 -0700

    Added art to buildings

--- OneButton/Assets/Scripts/BuildingCubeSettings.cs
+++ OneButton/Assets/Scripts/BuildingCubeSettings.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class BuildingCubeSettings : Destructible
 {
-    public BuildingData Data;
-    [SerializeField] private SpriteRenderer ThisSpriteRenderer;
+   
+    
 
     [Tooltip("Does this tile have a platform on it?")]
     [SerializeField] private bool HasPlatform;
 
     private GameObject platform;
 
-    [Space]
-    [Header("Sprite")]
-    [SerializeField] private int ActiveSpriteNumber;
-    [SerializeField] private Sprite ActiveSprite;
-    private void OnValidate()
-    {
-       
-        ActiveSpriteNumber = Mathf.Clamp(ActiveSpriteNumber, 0, Data.Sprites.Count -1);
-        ActiveSprite = Data.Sprites[ActiveSpriteNumber];
-    }
+    
+    
+   
     private void Awake()
     {
         platform = transform.GetChild(0).gameObject;
         platform.SetActive(HasPlatform);
-        ThisSpriteRenderer.sprite = ActiveSprite;
+        
     }
 
     
