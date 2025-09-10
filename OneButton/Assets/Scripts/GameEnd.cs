8891af0c7b37baea2a4dd38801d65e1b1ead4142
verycoolpotato <100438125+verycoolpotato@users.noreply.github.com>
2025-09-10 16:24:57 -0700

    Added game end functions

--- OneButton/Assets/Scripts/GameEnd.cs
+++ OneButton/Assets/Scripts/GameEnd.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameEnd : MonoBehaviour
 {
+    [Tooltip("Name of the scene used for gameplay")]
+    [SerializeField] string GameplaySceneName;
+
+    [Tooltip("Name of the scene used for MainMenu")]
+    [SerializeField] string MenuSceneName;
+
+    [SerializeField] 
+
 
     //Called by buttons
     public void ReturnToMenu()
     {
-
+        SceneManager.LoadScene(MenuSceneName);
     }
 
-
+   
     public void RetryGame()
     {
-
+        //hardcoded since there is only one gameplay scene
+        SceneManager.LoadScene(GameplaySceneName);
     }
 
     //Close Game
