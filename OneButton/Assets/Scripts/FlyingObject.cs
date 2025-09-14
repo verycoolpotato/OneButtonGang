884f1173d48f5d2a9680bba4b752a63bc32c3860
verycoolpotato <arandmagson@gmail.com>
2025-09-13 22:05:48 -0700

    Made flying objects script

---
+++ OneButton/Assets/Scripts/FlyingObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FlyingObject : Destructible
+{
+    [SerializeField] float MoveSpeed;
+    private void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(-0.1f * MoveSpeed, 0);
+
+        if (transform.position.x < -19)
+        {
+            transform.position = new Vector2(110, transform.position.y);
+        }
+    }
+}
